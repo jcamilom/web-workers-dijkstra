28d5e75c656758789c1acacf304d3578ab1bf8c7
Camilo Muñoz <jcamilomunozz@gmail.com>
2021-04-06 21:02:59 -0500

    Added data with coordinates

--- myscript.js
+++ myscript.js
@@ -1,14 +1,27 @@
 const width = 500;
 const height = 500;
-const data = [12, 15, 8];
+const graph = {
+  nodes: [
+    { x: 50, y: 250 },
+    { x: 100, y: 100 },
+    { x: 250, y: 80 },
+    { x: 330, y: 90 },
+    { x: 450, y: 280 },
+    { x: 320, y: 350 },
+    { x: 200, y: 390 },
+    { x: 150, y: 300 },
+    { x: 250, y: 180 },
+  ]
+}
 const svg = d3.select('body')
   .append('svg')
-  .attr('viewBox', [0, 0, width, height]);
+  .attr('width', width)
+  .attr('height', height);
 const node = svg
   .selectAll('.node')
-  .data(data)
+  .data(graph.nodes)
   .join('circle')
-    .attr('r', function (d, i) { return d; })
-    .attr('cy', 50)
-    .attr('cx', function (d, i) { return (i + 1) * 50; })
+    .attr('r', 10)
+    .attr('cy', function (d, i) { return d.y; })
+    .attr('cx', function (d, i) { return d.x; })
     .classed('node', true)
